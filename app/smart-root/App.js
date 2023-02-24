ed1c509b92f74e974b366261f472f02121ab59b5
k_chihogi <k_chihogi@ekty.co.jp>
2023-02-24 04:27:09 +0000

    ベースとなる箱を作成

--- app/smart-root/App.js
+++ app/smart-root/App.js
@@ -1,5 +1,12 @@
+import Constants from 'expo-constants';
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native';
 
 /**
  * App
@@ -7,18 +14,33 @@ import {StyleSheet, Text, View} from 'react-native';
  */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}></View>
+      <View style={styles.mapContainer}></View>
+      <View style={styles.footerContainer}></View>
+      <StatusBar style="light" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
+  },
+  inputContainer: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+  },
+  mapContainer: {
+    flex: 10,
+    width: Dimensions.get('window').width,
+  },
+  footerContainer: {
+    flex: 1,
+    width: Dimensions.get('window').width,
   },
 });
