6bac9f4c836341b0e2cd875a78776229b3ef8dfc
kchihogi <kchihogi@gmail.com>
2024-05-27 11:22:49 +0000

    feat: Add Expo config for Google Maps API key and update package dependencies

--- app/smart-root/app.config.ts
+++ app/smart-root/app.config.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import { ExpoConfig, ConfigContext } from '@expo/config';
+import appJson from './app.json';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
+  ...appJson.expo,
   extra: {
+    ...appJson.expo.extra,
     EXPO_PUBLIC_GOOGLE_MAP_API_KEY: process.env.EXPO_PUBLIC_GOOGLE_MAP_API_KEY,
   },
 });
