6bec01165f5f45319e081a248d848fd3304f7166
kchihogi <kchihogi@gmail.com>
2024-05-27 13:04:56 +0000

    chore: Update Expo config for Google Maps API key and package dependencies

--- app/smart-root/app.config.ts
+++ app/smart-root/app.config.ts
@@ -2,11 +2,30 @@ import 'dotenv/config';
 import { ExpoConfig, ConfigContext } from '@expo/config';
 import appJson from './app.json';
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
-  ...appJson.expo,
-  extra: {
-    ...appJson.expo.extra,
-    EXPO_PUBLIC_GOOGLE_MAP_API_KEY: process.env.EXPO_PUBLIC_GOOGLE_MAP_API_KEY,
-  },
-});
+export default ({ config }: ConfigContext): ExpoConfig => {
+
+  let key = process.env.EXPO_PUBLIC_GOOGLE_MAP_API_KEY;
+
+  return {
+    ...config,
+    ...appJson.expo,
+    android: {
+      ...config.android,
+      config: {
+        googleMaps: {
+          apiKey: key,
+        },
+      },
+    },
+    ios: {
+      ...config.ios,
+      config: {
+        googleMapsApiKey: key,
+      },
+    },
+    extra: {
+      ...appJson.expo.extra,
+      EXPO_PUBLIC_GOOGLE_MAP_API_KEY: key,
+    },
+  };
+};
